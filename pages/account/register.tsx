6ad65e37556d1389fed294210ed2a17b70502852
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2025-03-14 23:15:14 +0900

    fix: modified register page publishing

--- pages/account/register.tsx
+++ pages/account/register.tsx
@@ -1,42 +1,218 @@
-import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Box, Button, Checkbox, FormControlLabel, FormGroup,  Stack } from "@mui/material";
-import { NextPage } from "next";
-import Link from "next/link";
+import React, { useCallback, useState } from 'react';
+import { NextPage } from 'next';
+import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
+import withLayoutBasic from '../../libs/components/layout/LayoutBasic';
+import { Box, Button, Checkbox, FormControlLabel, FormGroup, Link, Stack } from '@mui/material';
+import { useRouter } from 'next/router';
 
-const Join:NextPage = () => {
-    return (
-        <Stack className={'join-page'}>
-            <Stack className="link-box"  spacing={1}>
-              <Box className={"link"}>
-                <Link href={"/"}>Home / </Link>
-              </Box>
-              <div className="detail-title">My Account</div>
-          </Stack>
-        <Stack className={'container'}>
-            <Stack className={'main'}>
-                <Box className={"logo-box"}>
-                     <img className={"logo"} src="/img/logo/favicon.svg" alt="favicon" />
-                    <span className="title">Nestwood</span>
-                </Box>
-                <Stack className="input-container">
-                    <p>Login</p>
-                    <Stack className="input-box">
-                        <span>Username</span>
-                        <input type="text" required={true} />
-                        <span>Password</span>
-                        <input type="password" required={true} />
-                    </Stack>
-                    <Box className={"btn-box"}>
-                    <Button variant="contained" className="btn">Login</Button>
-					    <FormGroup>
-						    <FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Remember me" />
-						</FormGroup>
-                </Box>
-                </Stack>
-            </Stack>
-        </Stack>
-    </Stack>
-    );
+
+// export const getStaticProps = async ({ locale }: any) => ({
+// 	props: {
+// 		...(await serverSideTranslations(locale, ['common'])),
+// 	},
+// });
+
+const Join: NextPage = () => {
+	const router = useRouter();
+	const device = useDeviceDetect();
+	const [input, setInput] = useState({ nick: '', password: '', phone: '', type: 'USER' });
+	const [loginView, setLoginView] = useState<boolean>(true);
+
+	/** HANDLERS **/
+	const viewChangeHandler = (state: boolean) => {
+		setLoginView(state);
+	};
+
+	const checkUserTypeHandler = (e: any) => {
+		const checked = e.target.checked;
+		if (checked) {
+			const value = e.target.name;
+			handleInput('type', value);
+		} else {
+			handleInput('type', 'USER');
+		}
+	};
+
+	const handleInput = useCallback((name: any, value: any) => {
+		setInput((prev) => {
+			return { ...prev, [name]: value };
+		});
+	}, []);
+
+	// const doLogin = useCallback(async () => {
+	// 	console.warn(input);
+	// 	try {
+	// 		await logIn(input.nick, input.password);
+	// 		await router.push(`${router.query.referrer ?? '/'}`);
+	// 	} catch (err: any) {
+	// 		await sweetMixinErrorAlert(err.message);
+	// 	}
+	// }, [input]);
+
+	// const doSignUp = useCallback(async () => {
+	// 	console.warn(input);
+	// 	try {
+	// 		await signUp(input.nick, input.password, input.phone, input.type);
+	// 		await router.push(`${router.query.referrer ?? '/'}`);
+	// 	} catch (err: any) {
+	// 		await sweetMixinErrorAlert(err.message);
+	// 	}
+	// }, [input]);
+
+	console.log('+input: ', input);
+
+	if (device === 'mobile') {
+		return <div>LOGIN MOBILE</div>;
+	} else {
+		return (
+			<Stack className={'join-page'}>
+                <Stack className="link-box"  spacing={1}>
+                    <Box className={"link"}>
+                        <Link href={"/"}>Home / </Link>
+                    </Box>
+                    <div className="detail-title">Blog</div>
+                 </Stack>
+				<Stack className={'container'}>
+					<Stack className={'main'}>
+						<Stack className={'left'}>
+							{/* @ts-ignore */}
+							<Box className={'logo'}>
+								<img src="/img/logo/favicon.svg" alt="" />
+								<span>Nestwood</span>
+							</Box>
+							<Box className={'info'}>
+								<span>{loginView ? 'login' : 'signup'}</span>
+								<p>{loginView ? 'Login' : 'Sign'} in with this account across the following sites.</p>
+							</Box>
+							<Box className={'input-wrap'}>
+								<div className={'input-box'}>
+									<span>Nickname</span>
+									<input
+										type="text"
+										placeholder={'Enter Nickname'}
+										onChange={(e) => handleInput('nick', e.target.value)}
+										required={true}
+										
+									/>
+								</div>
+								<div className={'input-box'}>
+									<span>Password</span>
+									<input
+										type="text"
+										placeholder={'Enter Password'}
+										onChange={(e) => handleInput('password', e.target.value)}
+										required={true}
+										// onKeyDown={(event) => {
+										// 	if (event.key == 'Enter' && loginView) doLogin();
+										// 	if (event.key == 'Enter' && !loginView) doSignUp();
+										// }}
+									/>
+								</div>
+								{!loginView && (
+									<div className={'input-box'}>
+										<span>Phone</span>
+										<input
+											type="text"
+											placeholder={'Enter Phone'}
+											onChange={(e) => handleInput('phone', e.target.value)}
+											required={true}
+											// onKeyDown={(event) => {
+											// 	if (event.key == 'Enter') doSignUp();
+											// }}
+										/>
+									</div>
+								)}
+							</Box>
+							<Box className={'register'}>
+								{!loginView && (
+									<div className={'type-option'}>
+										<span className={'text'}>I want to be registered as:</span>
+										<div>
+											<FormGroup>
+												<FormControlLabel
+													control={
+														<Checkbox
+															size="small"
+															name={'USER'}
+															onChange={checkUserTypeHandler}
+															checked={input?.type == 'USER'}
+														/>
+													}
+													label="User"
+												/>
+											</FormGroup>
+											<FormGroup>
+												<FormControlLabel
+													control={
+														<Checkbox
+															size="small"
+															name={'AGENT'}
+															onChange={checkUserTypeHandler}
+															checked={input?.type == 'AGENT'}
+														/>
+													}
+													label="Agent"
+												/>
+											</FormGroup>
+										</div>
+									</div>
+								)}
+
+								{loginView && (
+									<div className={'remember-info'}>
+										<FormGroup>
+											<FormControlLabel control={<Checkbox defaultChecked size="small" />} label="Remember me" />
+										</FormGroup>
+										<a>Lost your password?</a>
+									</div>
+								)}
+
+								{loginView ? (
+									<Button
+										variant="contained"
+										endIcon={<img src="/img/icons/rightup.svg" alt="" />}
+										disabled={input.nick == '' || input.password == ''}
+										
+									>
+										LOGIN
+									</Button>
+								) : (
+									<Button
+										variant="contained"
+										disabled={input.nick == '' || input.password == '' || input.phone == '' || input.type == ''}
+										
+										endIcon={<img src="/img/icons/rightup.svg" alt="" />}
+									>
+										SIGNUP
+									</Button>
+								)}
+							</Box>
+							<Box className={'ask-info'}>
+								{loginView ? (
+									<p>
+										Not registered yet?
+										<b
+											onClick={() => {
+												viewChangeHandler(false);
+											}}
+										>
+											SIGNUP
+										</b>
+									</p>
+								) : (
+									<p>
+										Have account?
+										<b onClick={() => viewChangeHandler(true)}> LOGIN</b>
+									</p>
+								)}
+							</Box>
+						</Stack>
+						<Stack className={'right'}></Stack>
+					</Stack>
+				</Stack>
+			</Stack>
+		);
+	}
 };
 
 export default withLayoutBasic(Join);
