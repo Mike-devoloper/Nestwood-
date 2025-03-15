5bf588d1544f32d87503a127258a43794cdc252a
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2025-03-16 01:48:03 +0900

    feat: develop agent screen page publishishing

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,21 +1,222 @@
-import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
-import { Stack } from "@mui/material";
-import { NextPage } from "next";
-import { useState } from "react";
+import React, { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
+import { NextPage } from 'next';
+import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
+import withLayoutBasic from '../../libs/components/layout/LayoutBasic';
+import { Stack, Box, Button, Pagination, Link } from '@mui/material';
+import { Menu, MenuItem } from '@mui/material';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import { useRouter } from 'next/router';
+import AgentCard from '@/libs/components/common/AgentCard';
 
-const AgentList: NextPage = () => {
-  const device = useDeviceDetect()
+// export const getStaticProps = async ({ locale }: any) => ({
+// 	props: {
+// 		...(await serverSideTranslations(locale, ['common'])),
+// 	},
+// });
 
-  if(device === "mobile") {
-     return <Stack>AGENT PAGE MOBILE</Stack>
-  } else { 
-    return (
-      <div style={{ margin: "20px 0" }}>
-        <Stack className="container">AGENTS LIST</Stack>
-      </div>
-    );
-  }
+const AgentList: NextPage = ({ initialInput, ...props }: any) => {
+	const device = useDeviceDetect();
+	const router = useRouter();
+	const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+	const [filterSortName, setFilterSortName] = useState('Recent');
+	const [sortingOpen, setSortingOpen] = useState(false);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	// const [searchFilter, setSearchFilter] = useState<any>(
+	// 	router?.query?.input ? JSON.parse(router?.query?.input as string) : initialInput,
+	// );
+	const [agents, setAgents] = useState<number[]>([1,2,3,4]);
+	const [total, setTotal] = useState<number>(0);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [searchText, setSearchText] = useState<string>('');
+
+	/** APOLLO REQUESTS **/
+	// const [likeTargetMember] = useMutation(LIKE_TARGET_MEMBER);
+
+	// const {
+	// 	loading: getAgentsLoading,
+	// 	data: getAgentsData,
+	// 	error: getAgentsError,
+	// 	refetch: getAgentsRefetch,
+	// } = useQuery(GET_AGENTS, {
+	// 	fetchPolicy: 'network-only',
+	// 	variables: { input: searchFilter },
+	// 	notifyOnNetworkStatusChange: true,
+	// 	onCompleted: (data: T) => {
+	// 		setAgents(data?.getAgents?.list);
+	// 		setTotal(data?.getAgents?.metaCounter[0]?.total);
+	// 	},
+	// });
+	/** LIFECYCLES **/
+	// useEffect(() => {
+	// 	if (router.query.input) {
+	// 		const input_obj = JSON.parse(router?.query?.input as string);
+	// 		setSearchFilter(input_obj);
+	// 	} else
+	// 		router.replace(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`);
+
+	// 	setCurrentPage(searchFilter.page === undefined ? 1 : searchFilter.page);
+	// }, [router]);
+
+	// /** HANDLERS **/
+	// const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
+	// 	setAnchorEl(e.currentTarget);
+	// 	setSortingOpen(true);
+	// };
+
+	// const sortingCloseHandler = () => {
+	// 	setSortingOpen(false);
+	// 	setAnchorEl(null);
+	// };
+
+	// const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+	// 	switch (e.currentTarget.id) {
+	// 		case 'recent':
+	// 			setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'DESC' });
+	// 			setFilterSortName('Recent');
+	// 			break;
+	// 		case 'old':
+	// 			setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'ASC' });
+	// 			setFilterSortName('Oldest order');
+	// 			break;
+	// 		case 'likes':
+	// 			setSearchFilter({ ...searchFilter, sort: 'memberLikes', direction: 'DESC' });
+	// 			setFilterSortName('Likes');
+	// 			break;
+	// 		case 'views':
+	// 			setSearchFilter({ ...searchFilter, sort: 'memberViews', direction: 'DESC' });
+	// 			setFilterSortName('Views');
+	// 			break;
+	// 	}
+	// 	setSortingOpen(false);
+	// 	setAnchorEl2(null);
+	// };
+
+	// const paginationChangeHandler = async (event: ChangeEvent<unknown>, value: number) => {
+	// 	searchFilter.page = value;
+	// 	await router.push(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`, {
+	// 		scroll: false,
+	// 	});
+	// 	setCurrentPage(value);
+	// };
+
+
+	// const likeMemberHandler = async (user: any, id: string) => {
+	// 	try {
+	// 		if (!id) return;
+	// 		if (!user._id) throw new Error(Messages.error2);
+
+	// 		await likeTargetMember({
+	// 			variables: {
+	// 				input: id,
+	// 			},
+	// 		});
+
+	// 		await getAgentsRefetch({ input: searchFilter });
+	// 		await sweetTopSmallSuccessAlert('success', 800);
+	// 	} catch (err: any) {
+	// 		sweetMixinErrorAlert(err.message).then();
+	// 	}
+	// };
+
+	if (device === 'mobile') {
+		return <h1>AGENTS PAGE MOBILE</h1>;
+	} else {
+		return (
+			<Stack className={'agent-list-page'}>
+                 <Stack className="link-box"  spacing={1}>
+                    <Box className={"link"}>
+                        <Link href={"/"}>Home / </Link>
+                    </Box>
+                    <div className="detail-title">Agent</div>
+                 </Stack>
+				<Stack className={'container'}>
+					<Stack className={'filter'}>
+						<Box component={'div'} className={'left'}>
+							<input
+								type="text"
+								placeholder={'Search for an agent'}
+								value={searchText}
+								onChange={(e: any) => setSearchText(e.target.value)}
+								// onKeyDown={(event: any) => {
+								// 	if (event.key == 'Enter') {
+								// 		setSearchFilter({
+								// 			...searchFilter,
+								// 			search: { ...searchFilter.search, text: searchText },
+								// 		});
+								// 	}
+								// }}
+							/>
+						</Box>
+						<Box component={'div'} className={'right'}>
+							<span>Sort by</span>
+							<div>
+								<Button endIcon={<KeyboardArrowDownRoundedIcon />}>
+									{filterSortName}
+								</Button>
+								<Menu anchorEl={anchorEl} open={sortingOpen}  sx={{ paddingTop: '5px' }}>
+									<MenuItem  id={'recent'} disableRipple>
+										Recent
+									</MenuItem>
+									<MenuItem  id={'old'} disableRipple>
+										Oldest
+									</MenuItem>
+									<MenuItem  id={'likes'} disableRipple>
+										Likes
+									</MenuItem>
+									<MenuItem  id={'views'} disableRipple>
+										Views
+									</MenuItem>
+								</Menu>
+							</div>
+						</Box>
+					</Stack>
+					<Stack className={'card-wrap'}>
+						{agents?.length === 0 ? (
+							<div className={'no-data'}>
+								<img src="/img/icons/icoAlert.svg" alt="" />
+								<p>No Agents found!</p>
+							</div>
+						) : (
+							agents.map((agent, index) => {
+								return <AgentCard agent={agent} key={agent} />;
+							})
+						)}
+					</Stack>
+					<Stack className={'pagination'}>
+						<Stack className="pagination-box">
+							{agents.length !== 0 && Math.ceil(8) > 1 && (
+								<Stack className="pagination-box">
+									<Pagination
+										page={currentPage}
+										count={Math.ceil(8)}
+								
+										shape="circular"
+										color="primary"
+									/>
+								</Stack>
+							)}
+						</Stack>
+
+						{agents.length !== 0 && (
+							<span>
+								Total {total} agent{total > 1 ? 's' : ''} available
+							</span>
+						)}
+					</Stack>
+				</Stack>
+			</Stack>
+		);
+	}
 };
+
+// AgentList.defaultProps = {
+// 	initialInput: {
+// 		page: 1,
+// 		limit: 10,
+// 		sort: 'createdAt',
+// 		direction: 'DESC',
+// 		search: {},
+// 	},
+// };
 
 export default withLayoutBasic(AgentList);
